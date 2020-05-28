01c50c662566b07dd5e536c7e83819989429614f
Qstevens35 <66077314+Qstevens35@users.noreply.github.com>
2020-05-28 11:23:36 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SearchAndSortAlgorithms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_Yes = new System.Windows.Forms.Label();
+            this.button_BinSerach = new System.Windows.Forms.Button();
+            this.button_SelecSort = new System.Windows.Forms.Button();
+            this.button_InsertSort = new System.Windows.Forms.Button();
+            this.button_MergeSort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_Yes
+            // 
+            this.label_Yes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label_Yes.Location = new System.Drawing.Point(12, 57);
+            this.label_Yes.Name = "label_Yes";
+            this.label_Yes.Size = new System.Drawing.Size(776, 384);
+            this.label_Yes.TabIndex = 0;
+            // 
+            // button_BinSerach
+            // 
+            this.button_BinSerach.Location = new System.Drawing.Point(30, 13);
+            this.button_BinSerach.Name = "button_BinSerach";
+            this.button_BinSerach.Size = new System.Drawing.Size(97, 23);
+            this.button_BinSerach.TabIndex = 1;
+            this.button_BinSerach.Text = "Binary Search";
+            this.button_BinSerach.UseVisualStyleBackColor = true;
+            this.button_BinSerach.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_SelecSort
+            // 
+            this.button_SelecSort.Location = new System.Drawing.Point(220, 13);
+            this.button_SelecSort.Name = "button_SelecSort";
+            this.button_SelecSort.Size = new System.Drawing.Size(101, 23);
+            this.button_SelecSort.TabIndex = 2;
+            this.button_SelecSort.Text = "Selection Sort";
+            this.button_SelecSort.UseVisualStyleBackColor = true;
+            this.button_SelecSort.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button_InsertSort
+            // 
+            this.button_InsertSort.Location = new System.Drawing.Point(419, 13);
+            this.button_InsertSort.Name = "button_InsertSort";
+            this.button_InsertSort.Size = new System.Drawing.Size(98, 23);
+            this.button_InsertSort.TabIndex = 3;
+            this.button_InsertSort.Text = "Insertion Sort";
+            this.button_InsertSort.UseVisualStyleBackColor = true;
+            this.button_InsertSort.Click += new System.EventHandler(this.button_InsertSort_Click);
+            // 
+            // button_MergeSort
+            // 
+            this.button_MergeSort.Location = new System.Drawing.Point(619, 13);
+            this.button_MergeSort.Name = "button_MergeSort";
+            this.button_MergeSort.Size = new System.Drawing.Size(75, 23);
+            this.button_MergeSort.TabIndex = 4;
+            this.button_MergeSort.Text = "Merge Sort";
+            this.button_MergeSort.UseVisualStyleBackColor = true;
+            this.button_MergeSort.Click += new System.EventHandler(this.button_MergeSort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Sort first*";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "New Array";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_MergeSort);
+            this.Controls.Add(this.button_InsertSort);
+            this.Controls.Add(this.button_SelecSort);
+            this.Controls.Add(this.button_BinSerach);
+            this.Controls.Add(this.label_Yes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Yes;
+        private System.Windows.Forms.Button button_BinSerach;
+        private System.Windows.Forms.Button button_SelecSort;
+        private System.Windows.Forms.Button button_InsertSort;
+        private System.Windows.Forms.Button button_MergeSort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace SearchAndSortAlgorithms
+{
+    public partial class Form1 : Form
+    {
+
+        int[] array = new int[6];
+        int searchval;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            searchval = Convert.ToInt32(textBox1.Text);
+            int min = 0;
+            int max = array.Length + 1;
+            int mid = ((min + max + 1) / 2);
+            int index = -1;
+
+            do
+            {
+                if (searchval == array[mid])
+                {
+                    index = mid;
+                    label_Yes.Text += "\n" + searchval.ToString() + " was found at position " + index.ToString();
+                }
+                else if (searchval < array[mid])
+                {
+                    max = mid - 1;
+                }
+                else if (searchval > array[mid])
+                {
+                    min = min + 1;
+                }
+                mid = ((min + max + 1) / 2);
+            } while ((min <= max) && (index == -1));
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int index;
+            int min;
+            int temp;
+            int smallestindex;
+            for (index = 0; index < array.Length; index++)
+            {
+                smallestindex = index;
+                for(min = index; min < array.Length; min++)
+                {
+                    if(array[min] < array[smallestindex])
+                    {
+                        smallestindex = min;
+                    }
+                }
+                temp = array[smallestindex];
+                array[smallestindex] = array[index];
+                array[index] = temp;
+                label_Yes.Text += "\n";
+                foreach (int num in array)
+                {
+                    label_Yes.Text += num.ToString() + " ";
+                }
+            }
+            label_Yes.Text += "\n" + "Sorted: ";
+            for (int ii = 0; ii < array.Length; ii++)
+            {
+                label_Yes.Text += array[ii].ToString() + " ";
+            }
+        }
+
+        private void button_InsertSort_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            int temp;
+            for(int ii = 1; ii <= array.Length - 1 ; ii++)
+            {
+                temp = array[ii];
+                index = ii - 1;
+                while (index >= 0 && array[index] > temp)
+                {
+                    array[index + 1] = array[index];
+                    index--;
+                }
+                array[index + 1] = temp;
+                label_Yes.Text += "\n";
+                for(int iii = 0; iii < array.Length; iii++)
+                {
+                    label_Yes.Text += array[iii] + " ";
+                }
+                index++;
+            }
+
+            label_Yes.Text += "\n" + "Sorted: ";
+            for (int iii = 0; iii < array.Length; iii++)
+            {
+                label_Yes.Text += array[iii] + " ";
+            }
+        }
+
+        private void button_MergeSort_Click(object sender, EventArgs e)
+        {
+            MergeSort(array, 0, array.Length - 1);
+            label_Yes.Text += "\n" + "Sorted: ";
+            for (int ii = 0; ii < array.Length; ii++)
+            {
+                label_Yes.Text += array[ii].ToString() + " ";
+            }
+        }
+
+        private void Merge(int[] input, int left, int right, int middle)
+        {
+
+            int temp;
+
+            for(int ii = middle + 1; ii <= right; ii++)
+            {
+                for(int kk = ii - 1; kk >= left; kk--)
+                {
+                    if(input[kk] > input[kk + 1])
+                    {
+                        temp = input[kk];
+                        input[kk] = input[kk + 1];
+                        input[kk + 1] = temp;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    label_Yes.Text += "\n";
+                    for (int bb = 0; bb < array.Length; bb++)
+                    {
+                        label_Yes.Text += array[bb].ToString() + " ";
+                    }
+                }
+            }
+        }
+
+        private void MergeSort(int[] input, int left, int right)
+        {
+            if (left == right)
+            {
+                return;
+            }
+            int middle = (left + right) / 2;
+            MergeSort(input, left, middle);
+            MergeSort(input, middle + 1, right);
+            Merge(input, left, right, middle);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random gen = new Random();
+            for (int ii = 0; ii < array.Length; ii++)
+            {
+                array[ii] = gen.Next(0, 100);
+            }
+            for(int ii = 0; ii < array.Length; ii++)
+            {
+                label_Yes.Text += " " + array[ii];
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Random gen = new Random();
+            for (int ii = 0; ii < array.Length; ii++)
+            {
+                array[ii] = gen.Next(0, 100);
+            }
+            label_Yes.Text += "\n";
+            for (int ii = 0; ii < array.Length; ii++)
+            {
+                label_Yes.Text += " " + array[ii];
+            }
+        }
+    }
+}
